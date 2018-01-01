6f957b1be5c01d2e0b7fcd904bed2d7bf82e62e8
bubbathevtog <bubbathevtog@gmail.com>
2018-01-01 03:48:38 -0500

    crossing things off of the TODO list

--- src/org.bubba.dla/Main.java
+++ src/org.bubba.dla/Main.java
@@ -5,92 +5,138 @@ import java.util.Vector;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 import javafx.scene.paint.Paint;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+
 
 //TODO:
-//fix error
+//comments -- done
+//fix error -- done
 //change colors
 //change size
-//infiniteWalkers - done
-//boundry checking
-//mousedrag listener
-//title
-//menu -> new && stop
+//infiniteWalkers -- done
+//boundry checking -- done
+//mousedrag listener -- done
+//title -- done
+//menu -> new,stop,exit -- WIP
 
 public class Main extends Application {
 
+	//Set our initial window size.
 	private int windowSize = 800;
 
-	private double movementFactor = 3.0;
-	private double walkerSize = 10.0;
-	private int concurrentWalkers = 800;
+	//change the properties of our walkers.
+	private double movementFactor = 5.0;
+	private double walkerSize = 5.0;
+	private int concurrentWalkers = 100;
 	private boolean infiniteWalkers = true;
 
+	//change the colors of the walkers.
 	private Color walkerColor = Color.AQUAMARINE;
 	private Color treeColor = Color.FIREBRICK;
 
+	//global scoped stuff we need.
 	private Canvas canvas;
 	private Random rand = new Random();
-
+	private AnimationTimer at;
 	private Vector<Walker> walkers = new Vector<Walker>();
+	private Vector<Walker> deadWalkers = new Vector<Walker>();
 	private Vector<Walker> tree = new Vector<Walker>();
 
+	//its main() yo.
 	public static void main(String[] args){
 		launch(args);
 	}
 
+	//start our application.
 	public void start(Stage window){
 
+		//this is our canvas.
 		canvas = new Canvas(windowSize,windowSize);
+		//this is our GraphicsContext.
 		GraphicsContext gcWalker = canvas.getGraphicsContext2D();
+		//not really needed, but better safe than sorry.
 		canvas.toFront();
 
+		//create our walkers.
 		for(int i=0; i<concurrentWalkers; i++){
 			walkers.add(new Walker((double)rand.nextInt((int)canvas.getWidth()),
 						(double)rand.nextInt((int)canvas.getHeight()),
 						walkerSize));
 		}
 
+		//our starting point.
 		Walker startNode = new Walker(canvas.getWidth()/2.0,canvas.getHeight()/2.0,walkerSize);
 		startNode.setFrozen();
 		tree.add(startNode);
 
+		//The pane that we are drawing on.
 		Pane pane = new Pane();
 		pane.setStyle("-fx-background-color: black");
 		pane.getChildren().addAll(canvas);
 
-		Scene scene = new Scene(pane,windowSize,windowSize);
+		//Menu stuff.
+		MenuBar menuBar = new MenuBar();
+		Menu menuMenu = new Menu("Menu");
+		MenuItem miExit = new MenuItem("Exit");
 
+		menuMenu.getItems().addAll(miExit);
+		menuBar.getMenus().addAll(menuMenu);
+
+		Scene scene = new Scene(pane,windowSize,windowSize);
+		//TODO: This is wicked broken, don't know if its just a linux thing.
+		((Pane)scene.getRoot()).getChildren().addAll(menuBar);
+
+		//Window size contants because of i3wm.
 		window.setMinWidth(windowSize);
 		window.setMaxWidth(windowSize);
 		window.setMinHeight(windowSize);
 		window.setMaxHeight(windowSize);
+		window.setTitle("Diffusion Limited Aggregation - By Bubba");
 		window.setScene(scene);
 		window.show();
 
-		new AnimationTimer(){
+		//the AnimationTimer we are using to control everything.
+		at = new AnimationTimer(){
 			@Override
 			public void handle(long now){
 				Main.this.update();
 				Main.this.draw(gcWalker);
 			}
-		}.start();
+		};
+		at.start();
+
+		//Click the canvas to add new walkers.
+		canvas.setOnMouseDragged(e -> {
+			walkers.add(new Walker(e.getX(),e.getY(),walkerSize));
+		});
+
+		miExit.setOnAction(e ->{
+			System.exit(0);
+		});
 	}
 
+	//update all of the particles inside of threads.
 	public void update(){
 		Vector<Thread> updateThreads = new Vector<Thread>();
+		//create our threads.
 		for(Walker w: walkers){
 			updateThreads.add(new Thread(w));
 		}
+		//start our threads.
 		for(Thread t: updateThreads){
 			t.start();
 		}
+		//wait for them to join back in.
 		for(Thread t: updateThreads){
 			try{
 				t.join();
@@ -98,7 +144,18 @@ public class Main extends Application {
 				System.out.println("An update thread was interrupted.");
 			}
 		}
+
+		//remove "dead" walkers so that we don't have to calculate stuff for them.
+		//this is an **attempt** at some optimization.
+		for(Walker d: deadWalkers){
+			walkers.remove(d);
+			tree.add(d);
+		}
+		deadWalkers.clear();
+
 		// System.out.println(""+walkers.get(0).getCenterX());
+
+		//add new walkers when others die.
 		if(infiniteWalkers){
 			for(int i=walkers.size(); i<concurrentWalkers; i++){
 				walkers.add(new Walker((double)rand.nextInt((int)canvas.getWidth()),
@@ -108,21 +165,28 @@ public class Main extends Application {
 		}
 	}
 
+	//Draw all of the things.
 	public void draw(GraphicsContext gc){
+		//clear our canvas every frame.
 		gc.clearRect(0,0,canvas.getWidth(),canvas.getHeight());
+		//set our walkers color.
 		gc.setFill(walkerColor);
+		//draw our walkers.
 		for(Walker w: walkers){
 			gc.fillOval(w.getCenterX(), w.getCenterY(),
 					w.getRadius(),w.getRadius());
 		}
 
+		//set our tree color.
 		gc.setFill(treeColor);
+		//draw our tree.
 		for(Walker t: tree){
 			gc.fillOval(t.getCenterX(), t.getCenterY(),
 					t.getRadius(),t.getRadius());
 		}
 	}
 
+	//NOT IMPLEMENTED YET....if ever...
 	public void shiftTreeColor(Walker walker){
 		double currentGreen = treeColor.getGreen();
 		double currentRed = treeColor.getRed();
@@ -136,32 +200,39 @@ public class Main extends Application {
 		double min = Math.min(minX,minY);
 	}
 
+	//Our walker class.
 	class Walker extends Circle implements Runnable{
 
 		private boolean frozen = false;
 
 		public Walker(double x, double y, double r){
 			super(x,y,r);
-			super.setFill(Paint.valueOf("blue"));
 		}
 
+		//this is only used once, for the initial tree particle.
 		public void setFrozen(){
 			frozen=true;
 		}
 
+		//return if the walker is "dead".
+		//this is basically used for some additional gating in other parts of the code.
 		private boolean isFrozen(){
 			return frozen;
 		}
 
+		//set our walker as frozen and mark it for removal in the Main.update().
 		private void setFrozenAndChangeState(){
 			frozen=true;
 			if(this.frozen){
-				tree.add(walkers.remove(walkers.indexOf(this)));
+				//tree.add(walkers.remove(walkers.indexOf(this)));
+				deadWalkers.add(walkers.get(walkers.indexOf((this))));
 			}
 		}
 
+		//Check if it collides with a tree particle.
 		private boolean collides(Walker otherWalker){
 			boolean collides = false;
+			//Distance calculations...wow!
 			double distance = Math.sqrt(
 					(this.getCenterX()-otherWalker.getCenterX())*(this.getCenterX()-otherWalker.getCenterX())+
 					(this.getCenterY()-otherWalker.getCenterY())*(this.getCenterY()-otherWalker.getCenterY())
@@ -173,27 +244,41 @@ public class Main extends Application {
 			return collides;
 		}
 
+		//This is what our thread runs duh.
+		@Override
 		public void run(){
 			this.update();
 		}
 
+		//all of our update code.
 		private void update(){
+			//if it isn't frozen...
 			if(!this.frozen){
+				//..set the center X value to something random between [-1,1] and scale it by our movementFactor.
 				this.setCenterX(rand.nextBoolean()?
 						((rand.nextDouble()*movementFactor)*-1)+this.getCenterX():
 						(rand.nextDouble()*movementFactor)+this.getCenterX()
 						);
 
-				super.setCenterY(rand.nextBoolean()?
+				//..set the center Y value to something random between [-1,1] and scale it by our movementFactor.
+				this.setCenterY(rand.nextBoolean()?
 						((rand.nextDouble()*movementFactor)*-1)+this.getCenterY():
 						(rand.nextDouble()*movementFactor)+this.getCenterY()
 						);
+
+				//if our particle is off of the canvas, loop it back to the other side.
+				if(this.getCenterX() > canvas.getWidth() || this.getCenterX() < 0 ||
+						this.getCenterY() > canvas.getHeight() || this.getCenterY() < 0){
+					this.setCenterX((this.getCenterX()+canvas.getWidth())%canvas.getHeight());
+					this.setCenterY((this.getCenterY()+canvas.getWidth())%canvas.getHeight());
+				}
 			}
 
+			//Check every walker in the tree, and if it collides with our particle, then change its state.
 			for(Walker t: tree){
 				if(this.collides(t)){
 					this.setFrozenAndChangeState();
-					Main.this.shiftTreeColor(t);
+					//Main.this.shiftTreeColor(t);
 				}
 			}
 		}
